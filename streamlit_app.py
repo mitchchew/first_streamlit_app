1123995345d45eed413396c314f7055088775448
mitchchew <94354815+mitchchew@users.noreply.github.com>
2023-08-20 22:50:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,7 @@
 import streamlit
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
 
 
 
