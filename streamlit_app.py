6415bd8c46fd7d0ecb221ee7fe13bca730dc2517
mitchchew <94354815+mitchchew@users.noreply.github.com>
2023-08-26 22:49:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ my_data_row = my_cur.fetchone()
 streamlit.text("The fruit list contains:")
 streamlit.text(my_data_row)
 
-add_my_fruit =
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
 
