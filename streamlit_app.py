2fa7abad5a78920bbbc5a3b82f7350575d1d35aa
mitchchew <94354815+mitchchew@users.noreply.github.com>
2023-08-30 11:54:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,3 +55,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit list contains:")
 streamlit.text(my_data_row)
+
+#this will not work correctly but just go with it 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
